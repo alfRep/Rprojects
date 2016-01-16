f0867e08cbdca31b7e25b581c530bae99cf6d418
alfRep <alf.email@gmail.com>
2016-01-16 00:48:33 -0600

    Initial

---
+++ Homework 1.R
@@ -0,0 +1,24 @@
+x <- c(100:109)
+
+y <- c(34, 28, 45, 67, 89, 93, 24, 49, 11, 7)
+
+x + y
+
+a <- 1.12; b <- 2.34; c <- 0.72; d <- 0.81; f <- 19.83
+
+x <- 1 + (a/b) + (c/f^2)
+x
+
+s <- (b-a)/(d-c)
+s
+
+r <- 1/((1/a) + (1/b) + (1/c) + (1/d))
+r
+
+y <- a*b *(1/c)*(f^2/2)
+y
+
+require(xlsx)
+HWData <- read.xlsx("HW01-Prob3Data.xlsx", sheetName = "autompg", header = TRUE)
+head(HWData)
+
