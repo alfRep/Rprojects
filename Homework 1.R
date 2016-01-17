465c7473e914decdd181d1b5c22f769f28f0610a
alfRep <alf.email@gmail.com>
2016-01-16 22:07:15 -0600

    Added comments

--- Homework 1.R
+++ Homework 1.R
@@ -1,11 +1,11 @@
+#Problem 1
 x <- c(100:109)
-
 y <- c(34, 28, 45, 67, 89, 93, 24, 49, 11, 7)
-
 x + y
 
-a <- 1.12; b <- 2.34; c <- 0.72; d <- 0.81; f <- 19.83
 
+#Problem 2
+a <- 1.12; b <- 2.34; c <- 0.72; d <- 0.81; f <- 19.83
 x <- 1 + (a/b) + (c/f^2)
 x
 
@@ -18,6 +18,8 @@ r
 y <- a*b *(1/c)*(f^2/2)
 y
 
+
+#Problem 3
 require(xlsx)
 HWData <- read.xlsx("HW01-Prob3Data.xlsx", sheetName = "autompg", header = TRUE)
 head(HWData)
